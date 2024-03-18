9a10473c6c4c54dcc1d34f057c0f09e032992dea
Andy Patari <andy.patari@gmail.com>
2024-03-18 11:18:26 -0400

    scrape with class

--- practice.py
+++ practice.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.backcountry.com/b/edelrid-boa-standard-climbing-rope-9.8mm?CMP_SKU=ELR002Y&MER=0406&skid=ELR002Y-BL-S40M&mr:device=c&mr:adType=plaonline&utm_source=google&utm_medium=pla&utm_campaign=20567682234__p:G%7Cs:BC%7Cct:Shopping%7Cct2:pmax%7Cg:xx%7Cc1:Climb%7Cc2:xx%7Cb:xx%7Cmt:xx____&utm_term=__ELR002Y-BL-S40M&utm_content=__pla&utm_id=go_cmp-20567682234_adg-_ad-__dev-c_ext-_prd-ELR002Y-BL-S40M_mca-7811_sig-CjwKCAjwzN-vBhAkEiwAYiO7oOB0sHISrWgaK0OJakK-bHcOZgncN7Ng-0StOdGKMghbW11OdSR5IBoCjwcQAvD_BwE&gad_source=1&gclid=CjwKCAjwzN-vBhAkEiwAYiO7oOB0sHISrWgaK0OJakK-bHcOZgncN7Ng-0StOdGKMghbW11OdSR5IBoCjwcQAvD_BwE&gclsrc=aw.ds"
-
+url = "https://www.blackdiamondequipment.com/en_US/product/8-5-dry-climbing-rope-past-season/?sku=BD323015GREN0501&gad_source=1&gclid=CjwKCAjwzN-vBhAkEiwAYiO7oHjuTDVGYYYGkxDgJkD0cAtXiLqGRkxzjs8Pvy_qAKknKjY2NHQTDRoC_VQQAvD_BwE&gclsrc=aw.ds"
 result = requests.get(url)
 
-print(result.text)
+doc = BeautifulSoup(result.text, "html.parser")
+
+
+prices = doc.find_all(class_="low-sale-price")
+
+print(prices[0].text)
