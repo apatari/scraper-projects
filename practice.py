f4b1451871a38f18debe533533c3cac08483cc1e
Andy Patari <andy.patari@gmail.com>
2024-03-18 13:37:32 -0400

    remove temporary comments

--- practice.py
+++ practice.py
@@ -8,12 +8,6 @@ def bcSearcher(param, max):
 
     doc = BeautifulSoup(result.text, "html.parser")
 
-    # def stripPrice(price):
-    #     try:
-    #         return (float(price.strip("$")))
-    #     except:
-    #         return 0 
-
     prices = doc.find_all(attrs={"data-id":"productListingPrice"})
 
     res = []
@@ -28,25 +22,7 @@ def bcSearcher(param, max):
 
     res.sort(key=lambda item: (item['priceFloat']))
 
-
-
     filteredResults = [item for item in res if item['priceFloat'] < max ]
 
     return filteredResults
 
-
-# url = f"https://www.backcountry.com/search?s=u&q=helmet"
-# result = requests.get(url)
-
-# doc = BeautifulSoup(result.text, "html.parser")
-# prices = doc.find_all(attrs={"data-id":"productListingPrice"})
-
-# res = []
-
-# priceString = prices[2].find_all(class_="chakra-text")[0].contents[-1]
-# priceFloat = float(priceString.strip("$"))
-
-# print(priceFloat)
-
-
-# print(float(test[0].contents[-1].strip("$")))
