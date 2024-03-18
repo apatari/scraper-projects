38bab78b3f76d713424c9e640ebd7ccbcc9ae294
Andy Patari <andy.patari@gmail.com>
2024-03-18 13:36:47 -0400

    handle both price types, update result dict

--- practice.py
+++ practice.py
@@ -8,8 +8,11 @@ def bcSearcher(param, max):
 
     doc = BeautifulSoup(result.text, "html.parser")
 
-    def stripPrice(price):
-        return (float(price.strip("$")))
+    # def stripPrice(price):
+    #     try:
+    #         return (float(price.strip("$")))
+    #     except:
+    #         return 0 
 
     prices = doc.find_all(attrs={"data-id":"productListingPrice"})
 
@@ -18,13 +21,32 @@ def bcSearcher(param, max):
     for price in prices:
         name = price.parent.parent.parent.find("h2", attrs={"data-id": "productListingTitle"})
 
-        highPrice = price.find("span")
-        res.append({"name": name.text, "price":highPrice.text})
+        priceString = price.find_all(class_="chakra-text")[0].contents[-1]
+        priceFloat = float(priceString.strip("$"))
 
-    res.sort(key=lambda item: stripPrice(item['price']))
+        res.append({"name": name.text, "priceString":priceString, "priceFloat":priceFloat})
+
+    res.sort(key=lambda item: (item['priceFloat']))
 
 
 
-    filteredResults = [item for item in res if stripPrice(item['price']) < max ]
+    filteredResults = [item for item in res if item['priceFloat'] < max ]
 
     return filteredResults
+
+
+# url = f"https://www.backcountry.com/search?s=u&q=helmet"
+# result = requests.get(url)
+
+# doc = BeautifulSoup(result.text, "html.parser")
+# prices = doc.find_all(attrs={"data-id":"productListingPrice"})
+
+# res = []
+
+# priceString = prices[2].find_all(class_="chakra-text")[0].contents[-1]
+# priceFloat = float(priceString.strip("$"))
+
+# print(priceFloat)
+
+
+# print(float(test[0].contents[-1].strip("$")))
--- projects.py
+++
@@ -1,2 +0,0 @@
-# see the backcountry.com example for climbing ropes
-# try letting users put in the search term, and maybe even a price limit?
