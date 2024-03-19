4acb9dab2091f199b70a362a71d2ec635a556340
Andy Patari <andy.patari@gmail.com>
2024-03-19 13:19:24 -0400

    include product slug

--- practice.py
+++ practice.py
@@ -15,10 +15,13 @@ def bcSearcher(param, max, min):
     for price in prices:
         name = price.parent.parent.parent.find("h2", attrs={"data-id": "productListingTitle"})
 
+        anchor = price.parent.parent.parent.parent.find("a", class_="chakra-linkbox__overlay")
+        link = anchor.attrs['href']
+
         priceString = price.find_all(class_="chakra-text")[0].contents[-1]
         priceFloat = float(priceString.strip("$").replace(",",""))
 
-        res.append({"name": name.text, "priceString":priceString, "priceFloat":priceFloat})
+        res.append({"name": name.text, "priceString":priceString, "priceFloat":priceFloat, "link": link})
 
     res.sort(key=lambda item: (item['priceFloat']))
 
