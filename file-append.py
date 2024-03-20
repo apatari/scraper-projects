c762e814b1e42c7675dc37c62520e86b11e41203
Andy Patari <andy.patari@gmail.com>
2024-03-20 08:17:36 -0400

    add name == main

--- file-append.py
+++ file-append.py
@@ -1,7 +1,10 @@
 import datetime
 
 time = datetime.datetime.now()
+def timestamp():
+    f = open("myfile.txt", "a")
+    f.write(f"testing, {time} \n")
+    f.close()
 
-f = open("myfile.txt", "a")
-f.write(f"testing, {time} \n")
-f.close()
+if __name__ == "__main__":
+    timestamp()
