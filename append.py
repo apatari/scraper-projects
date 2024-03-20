2ca2fe37f9eb4364c32c667552992cce232a677b
Andy Patari <andy.patari@gmail.com>
2024-03-20 09:32:45 -0400

    simple crontab script functional

--- append.py
+++ append.py
@@ -2,7 +2,7 @@ import datetime
 
 time = datetime.datetime.now()
 def timestamp():
-    f = open("myfile.txt", "a")
+    f = open("/Users/andypatari/Development/code/post-projects/scraper-projects/myfile.txt", "a")
     f.write(f"testing, {time} \n")
     f.close()
 
--- projects.md
+++
@@ -1,5 +0,0 @@
-see the backcountry.com example for climbing ropes
-
-try letting users put in the search term, and maybe even a price limit?
-
-look into removing the "Current price: " from the prices
