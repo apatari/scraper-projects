2f1ac490495036b003a36534c44848d15f6bea71
Andy Patari <andy.patari@gmail.com>
2024-03-20 10:48:49 -0400

    create send fn

--- slackBot.py
+++ slackBot.py
@@ -2,15 +2,20 @@ from dotenv import load_dotenv
 from slack_sdk import WebClient
 import os
 
-load_dotenv()
+def send(msg):
+    load_dotenv()
 
-msg = "testing..."
+    # msg = ""
+    # l = [{"a": "b", "c":"d"}, {"a": "r", "ct":"y"}]
+    # for item in l:
+    #     msg += (str(item['a']) + '\n')
 
-client = WebClient(token=os.environ.get("SLACK_TOKEN"))
 
-client.chat_postMessage(
-    channel="python-messages",
-    text=msg,
-    username="@Scraper"
-)
+    client = WebClient(token=os.environ.get("SLACK_TOKEN"))
+
+    client.chat_postMessage(
+        channel="python-messages",
+        text=msg,
+        username="@Scraper"
+    )
 
