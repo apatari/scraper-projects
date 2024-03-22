881d1fa9eab5a9b596ca70681919ef89d0a67e2d
Andy Patari <andy.patari@gmail.com>
2024-03-22 09:45:48 -0400

    bcBot prints properly formatted string

--- bcBot.py
+++ bcBot.py
@@ -4,3 +4,13 @@
 from practice import bcMultiPage
 from slackBot import sendToSlack
 
+if __name__ == "__main__":
+    items = bcMultiPage("climbing rope", 200, 210)
+
+    msg = ""
+
+    for item in items:
+        line = ""
+        line += f"{item['name']} Price: {item['priceString']} \n"
+        msg += line
+    print(msg)
